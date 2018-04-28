451c82333d89d3474725fbbc6f535a18d111608f
amanda.yang93 <amanda.yang@titansoft.com.sg>
2018-04-28 11:49:08 +0800

    [泛型] 解決<> 傳入型別寫死的問題 [Func] 解決再呼叫端寫死條件參數的問題 [IEnumerable]
    優點 增加彈性
    擴增方法有四大條件
    statis method and class
    使用namespace包
    第一個參數加 this

--- LinqTests/UnitTest1.cs
+++ LinqTests/UnitTest1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Amanda;
 using ExpectedObjects;
 using LinqTests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LinqTests
 {
@@ -13,7 +15,7 @@ namespace LinqTests
         public void find_products_that_price_between_200_and_500()
         {
             var products = RepositoryFactory.GetProducts();
-            var actual = WithoutLinq.FindProductByPrice(products, 200, 500,"Odd-e");
+            var actual = WithoutLinq.FindProductByPrice(products, 200, 500, "Odd-e");
 
             var expected = new List<Product>()
             {
@@ -25,15 +27,47 @@ namespace LinqTests
         }
 
         [TestMethod]
-        public void find_products_that_price_between_200_and_500_Linq()
+        public void find_products_that_price_between_200_and_500_UseMyWhere()
         {
             var products = RepositoryFactory.GetProducts();
-            var actual = YourOwnLinq.MyWhere(products, p=>p.Price >= 200 && p.Price <= 500 && p.Supplier == "Odd-e");
+            var actual = products.MyWhere(p => p.Price >= 200 && p.Price <= 500 && p.Supplier == "Odd-e").ToList();
 
             var expected = new List<Product>()
             {
                 new Product{Id=3, Cost=31, Price=310, Supplier="Odd-e" },
                 new Product{Id=4, Cost=41, Price=410, Supplier="Odd-e" }
+            };
+
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+
+        [TestMethod]
+        public void find_products_that_price_between_200_and_500_UseLinqWhere()
+        {
+            var products = RepositoryFactory.GetProducts();
+            var actual = products.Where(p => p.Price >= 200 && p.Price <= 500 && p.Supplier == "Odd-e").ToList();
+
+            var expected = new List<Product>()
+            {
+                new Product{Id=3, Cost=31, Price=310, Supplier="Odd-e" },
+                new Product{Id=4, Cost=41, Price=410, Supplier="Odd-e" }
+            };
+
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+
+        [TestMethod]
+        public void find_Employee_that_age_between_25_and_45_ForEmployee()
+        {
+            var employees = RepositoryFactory.GetEmployees();
+            var actual = employees.MyWhere(p => p.Age >= 25 && p.Age <= 40).ToList();
+
+            var expected = new List<Employee>()
+            {
+                new Employee{Name="Tom", Role=RoleType.Engineer, MonthSalary=140, Age=33, WorkingYear=2.6} ,
+                new Employee{Name="Bas", Role=RoleType.Engineer, MonthSalary=280, Age=36, WorkingYear=2.6} ,
+                new Employee{Name="Mary", Role=RoleType.OP, MonthSalary=180, Age=26, WorkingYear=2.6} ,
+                new Employee{Name="Joey", Role=RoleType.Engineer, MonthSalary=250, Age=40, WorkingYear=2.6}
             };
 
             expected.ToExpectedObject().ShouldEqual(actual);
@@ -48,29 +82,41 @@ internal class WithoutLinq
         var productList = new List<Product>();
         foreach (var product in products)
         {
-           
             if (product.Price >= lowBoundary && product.Price <= highBoundary && product.Supplier == supplier)
             {
                 productList.Add(product);
             }
         }
-       
+
         return productList;
     }
 }
 
-internal class YourOwnLinq
+namespace Amanda
 {
-    public static List<Product> MyWhere(IEnumerable<Product> products, Func<Product, bool> func)
+    internal static class YourOwnLinq
     {
-        var result =new List<Product>();
-        foreach (var product in products)
+        public static IEnumerable<TSource> MyWhere<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> func)
         {
-            if (func(product))
+            //var result =new List<Product>();
+            //foreach (var product in products)
+            //{
+            //    if (func(product))
+            //    {
+            //        result.Add(product);
+            //    }
+            //}
+            //return result;
+
+            var result = source.GetEnumerator();
+            while (result.MoveNext())
             {
-                result.Add(product);
+                var itme = result.Current;
+                if (func(itme))
+                {
+                    yield return itme;
+                }
             }
         }
-        return result;
     }
 }
