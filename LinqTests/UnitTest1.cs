028a9f73197aa958e99d1e3eca14969dcc166af9
amanda.yang93 <amanda.yang@titansoft.com.sg>
2018-04-28 15:57:19 +0800

    Move WithoutLinq to another file

--- LinqTests/UnitTest1.cs
+++ LinqTests/UnitTest1.cs
@@ -1,5 +1,4 @@
 ﻿using ExpectedObjects;
-using LinqTests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Linq;
@@ -219,37 +218,5 @@ namespace LinqTests
             expected.ToExpectedObject().ShouldEqual(actual.ToList());
         }
 
-    }
-}
-
-internal static class WithoutLinq
-{
-    public static List<Product> FindProductByPrice(IEnumerable<Product> products, int lowBoundary, int highBoundary, string supplier)
-    {
-        var productList = new List<Product>();
-        foreach (var product in products)
-        {
-            if (product.Price >= lowBoundary && product.Price <= highBoundary && product.Supplier == supplier)
-            {
-                productList.Add(product);
-            }
-        }
-
-        return productList;
-    }
-
-    public static IEnumerable<string> ChangeTo91Port(IEnumerable<string> urls)
-    {
-        foreach (var urlItem in urls)
-        {
-            if (urlItem.Contains("tw"))
-            {
-                yield return urlItem + ":91";
-            }
-            else
-            {
-                yield return urlItem;
-            }
-        }
     }
 }
---
+++ LinqTests/WithoutLinq.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using LinqTests;
+
+internal class WithoutLinq
+{
+    public static List<Product> FindProductByPrice(IEnumerable<Product> products, int lowBoundary, int highBoundary, string supplier)
+    {
+        var productList = new List<Product>();
+        foreach (var product in products)
+        {
+            if (product.Price >= lowBoundary && product.Price <= highBoundary && product.Supplier == supplier)
+            {
+                productList.Add(product);
+            }
+        }
+
+        return productList;
+    }
+
+    public static IEnumerable<string> ChangeTo91Port(IEnumerable<string> urls)
+    {
+        foreach (var urlItem in urls)
+        {
+            if (urlItem.Contains("tw"))
+            {
+                yield return urlItem + ":91";
+            }
+            else
+            {
+                yield return urlItem;
+            }
+        }
+    }
+}
