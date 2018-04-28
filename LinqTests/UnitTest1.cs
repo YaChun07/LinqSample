4b0a501acb0625b5fe2a50c5c5a1432e38a40289
amanda.yang93 <amanda.yang@titansoft.com.sg>
2018-04-28 15:55:25 +0800

    Move YourOwnLinq to another file

--- LinqTests/UnitTest1.cs
+++ LinqTests/UnitTest1.cs
@@ -1,8 +1,6 @@
-﻿using Amanda;
-using ExpectedObjects;
+﻿using ExpectedObjects;
 using LinqTests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -251,106 +249,6 @@ internal static class WithoutLinq
             else
             {
                 yield return urlItem;
-            }
-        }
-    }
-}
-
-namespace Amanda
-{
-    internal static class YourOwnLinq
-    {
-        public static IEnumerable<TSource> MyWhere<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> func)
-        {
-            var result = source.GetEnumerator();
-            while (result.MoveNext())
-            {
-                var itme = result.Current;
-                if (func(itme))
-                {
-                    yield return itme;
-                }
-            }
-        }
-
-        public static IEnumerable<TResult> MySelect<TSource, TResult>(this IEnumerable<TSource> urls, Func<TSource, TResult> selector)
-        {
-            foreach (var urlItem in urls)
-            {
-                yield return selector(urlItem);
-            }
-        }
-
-        public static IEnumerable<TSource> MySkip<TSource>(this IEnumerable<TSource> sources, int topN)
-        {
-            int count = 0;
-            foreach (var source in sources)
-            {
-                if (count >= topN)
-                {
-                    yield return source;
-                }
-                count++;
-            }
-        }
-
-        public static IEnumerable<TSource> MySkipWhile<TSource>(this IEnumerable<TSource> sources, Func<TSource, bool> selector,int skipNum)
-        {
-            var enumerator = sources.GetEnumerator();
-            var count = 0;
-            while (enumerator.MoveNext())
-            {
-                if (count < skipNum && selector(enumerator.Current))
-                {
-                    count++;
-                }
-                else
-                {
-                    yield return enumerator.Current;
-                }
-               
-            }
-        }
-
-        public static IEnumerable<TResult> MySelect<TSource, TResult>(this IEnumerable<TSource> urls, Func<TSource, int, TResult> selector)
-        {
-            var count = 0;
-            foreach (var urlItem in urls)
-            {
-                yield return selector(urlItem, count);
-                count++;
-            }
-        }
-
-        public static IEnumerable<TSource> MyTake<TSource>(this IEnumerable<TSource> sources, int topN)
-        {
-            var enumerator = sources.GetEnumerator();
-            var count = 0;
-            while (enumerator.MoveNext())
-            {
-                if (count < topN)
-                {
-                    yield return enumerator.Current;
-                }
-                else
-                {
-                    yield break;
-                }
-                count++;
-            }
-        }
-
-        public static IEnumerable<TSource> MyTakeWhile<TSource>(this IEnumerable<TSource> sources, Func<TSource, bool> selector, int takeNum)
-        {
-            var enumerator = sources.GetEnumerator();
-            var count = 0;
-            while (enumerator.MoveNext())
-            {
-                if (selector(enumerator.Current) && count < takeNum)
-                {
-                    yield return enumerator.Current;
-                    count++;
-                }
             }
         }
     }
