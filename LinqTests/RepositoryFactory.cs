a8fc9bc3a5d6c541653a6800d57b05e2a1403352
amanda.yang93 <amanda.yang@titansoft.com.sg>
2018-06-23 11:59:41 +0800

    add GetBalls

--- LinqTests/RepositoryFactory.cs
+++ LinqTests/RepositoryFactory.cs
@@ -2,14 +2,49 @@
 
 namespace LinqTests
 {
+    internal enum Color
+    {
+        Purple,
+        Blue,
+        Yellow,
+        Green
+    }
+
+    internal class ColorBall
+    {
+        public Color Color { get; set; }
+        public int Prize { get; set; }
+        public string Size { get; set; }
+    }
+
     internal class RepositoryFactory
     {
-        public static IEnumerable<string> GetUrls()
+        public static IEnumerable<ColorBall> GetBalls()
         {
-            yield return "http://tw.yahoo.com";
-            yield return "https://facebook.com";
-            yield return "https://twitter.com";
-            yield return "http://github.com";
+            return new List<ColorBall>
+            {
+                new ColorBall{Color=Color.Blue, Size="L", Prize=200},
+                new ColorBall{Color=Color.Purple, Size="L", Prize=350},
+                new ColorBall{Color=Color.Yellow, Size="S", Prize=300},
+                new ColorBall{Color=Color.Purple, Size="S", Prize=500},
+                new ColorBall{Color=Color.Purple, Size="L", Prize=450},
+                new ColorBall{Color=Color.Yellow, Size="M", Prize=500},
+            };
+        }
+
+        public static IEnumerable<Employee> GetEmployees()
+        {
+            return new List<Employee>
+            {
+                new Employee{Name="Joe", Role=RoleType.Engineer, MonthSalary=100, Age=44, WorkingYear=2.6 } ,
+                new Employee{Name="Tom", Role=RoleType.Engineer, MonthSalary=140, Age=33, WorkingYear=2.6} ,
+                new Employee{Name="Kevin", Role=RoleType.Manager, MonthSalary=380, Age=55, WorkingYear=2.6} ,
+                new Employee{Name="Andy", Role=RoleType.OP, MonthSalary=80, Age=22, WorkingYear=2.6} ,
+                new Employee{Name="Bas", Role=RoleType.Engineer, MonthSalary=280, Age=36, WorkingYear=2.6} ,
+                new Employee{Name="Mary", Role=RoleType.OP, MonthSalary=180, Age=26, WorkingYear=2.6} ,
+                new Employee{Name="Frank", Role=RoleType.Engineer, MonthSalary=120, Age=16, WorkingYear=2.6} ,
+                new Employee{Name="Joey", Role=RoleType.Engineer, MonthSalary=250, Age=40, WorkingYear=2.6},
+            };
         }
 
         public static IEnumerable<Product> GetProducts()
@@ -27,19 +62,12 @@ namespace LinqTests
             };
         }
 
-        public static IEnumerable<Employee> GetEmployees()
+        public static IEnumerable<string> GetUrls()
         {
-            return new List<Employee>
-            {
-                new Employee{Name="Joe", Role=RoleType.Engineer, MonthSalary=100, Age=44, WorkingYear=2.6 } ,
-                new Employee{Name="Tom", Role=RoleType.Engineer, MonthSalary=140, Age=33, WorkingYear=2.6} ,
-                new Employee{Name="Kevin", Role=RoleType.Manager, MonthSalary=380, Age=55, WorkingYear=2.6} ,
-                new Employee{Name="Andy", Role=RoleType.OP, MonthSalary=80, Age=22, WorkingYear=2.6} ,
-                new Employee{Name="Bas", Role=RoleType.Engineer, MonthSalary=280, Age=36, WorkingYear=2.6} ,
-                new Employee{Name="Mary", Role=RoleType.OP, MonthSalary=180, Age=26, WorkingYear=2.6} ,
-                new Employee{Name="Frank", Role=RoleType.Engineer, MonthSalary=120, Age=16, WorkingYear=2.6} ,
-                new Employee{Name="Joey", Role=RoleType.Engineer, MonthSalary=250, Age=40, WorkingYear=2.6},
-            };
+            yield return "http://tw.yahoo.com";
+            yield return "https://facebook.com";
+            yield return "https://twitter.com";
+            yield return "http://github.com";
         }
     }
 }
