685b463737f4d9567a9b255c6675ae9d8f9497a5
amanda.yang93 <amanda.yang@titansoft.com.sg>
2018-06-23 10:53:37 +0800

    20180623

--- LinqTests/UnitTest1.cs
+++ LinqTests/UnitTest1.cs
@@ -9,25 +9,59 @@ namespace LinqTests
     public class UnitTest1
     {
         [TestMethod]
-        public void find_products_that_price_between_200_and_500()
+        public void All()
         {
             var products = RepositoryFactory.GetProducts();
-            var actual = WithoutLinq.FindProductByPrice(products, 200, 500, "Odd-e");
+            var actual = products.MyAll(p => p.Price > 200);
 
-            var expected = new List<Product>()
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void AnyHasRecord()
+        {
+            var products = RepositoryFactory.GetProducts();
+            var actual = products.MyAny();
+
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void Distinct()
+        {
+            var employees = RepositoryFactory.GetEmployees();
+            var actual = employees.MySelect(p => p.Role).MyDistinct();
+            var expected = new List<RoleType>
             {
-                new Product{Id=3, Cost=31, Price=310, Supplier="Odd-e" },
-                new Product{Id=4, Cost=41, Price=410, Supplier="Odd-e" }
+                RoleType.Engineer,
+                RoleType.OP,
+                RoleType.Manager
+            };
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+        }
+
+        [TestMethod]
+        public void find_Employee_that_age_between_25_and_45_ForEmployee()
+        {
+            var employees = RepositoryFactory.GetEmployees();
+            var actual = employees.MyWhere(p => p.Age >= 25 && p.Age <= 40).ToList();
+
+            var expected = new List<Employee>()
+            {
+                new Employee{Name="Tom", Role=RoleType.Engineer, MonthSalary=140, Age=33, WorkingYear=2.6} ,
+                new Employee{Name="Bas", Role=RoleType.Engineer, MonthSalary=280, Age=36, WorkingYear=2.6} ,
+                new Employee{Name="Mary", Role=RoleType.OP, MonthSalary=180, Age=26, WorkingYear=2.6} ,
+                new Employee{Name="Joey", Role=RoleType.Engineer, MonthSalary=250, Age=40, WorkingYear=2.6}
             };
 
             expected.ToExpectedObject().ShouldEqual(actual);
         }
 
         [TestMethod]
-        public void find_products_that_price_between_200_and_500_UseMyWhere()
+        public void find_products_that_price_between_200_and_500()
         {
             var products = RepositoryFactory.GetProducts();
-            var actual = products.MyWhere(p => p.Price >= 200 && p.Price <= 500 && p.Supplier == "Odd-e").ToList();
+            var actual = WithoutLinq.FindProductByPrice(products, 200, 500, "Odd-e");
 
             var expected = new List<Product>()
             {
@@ -54,71 +88,18 @@ namespace LinqTests
         }
 
         [TestMethod]
-        public void find_Employee_that_age_between_25_and_45_ForEmployee()
+        public void find_products_that_price_between_200_and_500_UseMyWhere()
         {
-            var employees = RepositoryFactory.GetEmployees();
-            var actual = employees.MyWhere(p => p.Age >= 25 && p.Age <= 40).ToList();
+            var products = RepositoryFactory.GetProducts();
+            var actual = products.MyWhere(p => p.Price >= 200 && p.Price <= 500 && p.Supplier == "Odd-e").ToList();
 
-            var expected = new List<Employee>()
+            var expected = new List<Product>()
             {
-                new Employee{Name="Tom", Role=RoleType.Engineer, MonthSalary=140, Age=33, WorkingYear=2.6} ,
-                new Employee{Name="Bas", Role=RoleType.Engineer, MonthSalary=280, Age=36, WorkingYear=2.6} ,
-                new Employee{Name="Mary", Role=RoleType.OP, MonthSalary=180, Age=26, WorkingYear=2.6} ,
-                new Employee{Name="Joey", Role=RoleType.Engineer, MonthSalary=250, Age=40, WorkingYear=2.6}
+                new Product{Id=3, Cost=31, Price=310, Supplier="Odd-e" },
+                new Product{Id=4, Cost=41, Price=410, Supplier="Odd-e" }
             };
 
             expected.ToExpectedObject().ShouldEqual(actual);
-        }
-
-        [TestMethod]
-        public void ToHttps()
-        {
-            var urls = RepositoryFactory.GetUrls();
-            var actual = urls.MySelect(urlItem => urlItem.Replace("http:", "https:").Replace(".com", ".com:8080"));
-
-            var expected = new List<string>
-            {
-                "https://tw.yahoo.com:8080",
-                "https://facebook.com:8080",
-                "https://twitter.com:8080",
-                "https://github.com:8080"
-            };
-
-            expected.ToExpectedObject().ShouldEqual(actual.ToList());
-        }
-
-        [TestMethod]
-        public void UrlLength()
-        {
-            var urls = RepositoryFactory.GetUrls();
-            var actual = urls.MySelect(urlItem => urlItem.Length);
-
-            var expected = new List<int>
-            {
-                19,
-                20,
-                19,
-                17
-            };
-
-            expected.ToExpectedObject().ShouldEqual(actual.ToList());
-        }
-
-        [TestMethod]
-        public void To91Port_IfContainstw()
-        {
-            var urls = RepositoryFactory.GetUrls();
-            var actual = WithoutLinq.ChangeTo91Port(urls);
-
-            var expected = new List<string>
-            {
-                "http://tw.yahoo.com:91",
-                "https://facebook.com",
-                "https://twitter.com:91",
-                "http://github.com"
-            };
-
-            expected.ToExpectedObject().ShouldEqual(actual.ToList());
         }
 
         [TestMethod]
@@ -138,6 +119,44 @@ namespace LinqTests
             };
 
             expected.ToExpectedObject().ShouldEqual(actual.ToList());
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void Group_3_AndSum()
+        {
+            var products = RepositoryFactory.GetProducts();
+            var actual = products.GroupSum(3, p => p.Price);
+
+            var expected = new List<int>
+            {
+                630,1530,1490
+            };
+
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void Group_5_AndSum()
+        {
+            var products = RepositoryFactory.GetProducts();
+            var actual = products.GroupSum(5, p => p.Price);
+
+            var expected = new List<int>
+            {
+                1550,2100
+            };
+
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+        }
+
+        [TestMethod]
+        public void Is_AnyEngineer_Above_45()
+        {
+            var employees = RepositoryFactory.GetEmployees();
+            var actual = employees.IsAnyEngeerAbove45(employee => employee.Age > 45 && employee.Role == RoleType.Engineer);
+            Assert.IsFalse(actual);
         }
 
         [TestMethod]
@@ -180,7 +199,6 @@ namespace LinqTests
             {
                 new Employee{Name="Frank", Role=RoleType.Engineer, MonthSalary=120, Age=16, WorkingYear=2.6} ,
                 new Employee{Name="Joey", Role=RoleType.Engineer, MonthSalary=250, Age=40, WorkingYear=2.6},
-
             };
 
             expected.ToExpectedObject().ShouldEqual(actual.ToList());
@@ -190,7 +208,7 @@ namespace LinqTests
         public void Skip6MoreThan300()
         {
             var products = RepositoryFactory.GetProducts();
-            var actual = products.MySkipWhile(e=>e.Price>300, 4);
+            var actual = products.MySkipWhile(e => e.Price > 300, 4);
 
             var expected = new List<Product>
             {
@@ -202,7 +220,7 @@ namespace LinqTests
 
             expected.ToExpectedObject().ShouldEqual(actual.ToList());
         }
-        
+
         [TestMethod]
         public void SkipMoreThan300()
         {
@@ -222,6 +240,16 @@ namespace LinqTests
             expected.ToExpectedObject().ShouldEqual(actual.ToList());
         }
 
+        [TestMethod]
+        public void Sum()
+        {
+            var products = RepositoryFactory.GetProducts();
+            var actual = products.MySum(p => p.Price);
+
+            var expected = 3650;
+
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
 
         [TestMethod]
         public void Take2MoreThan300()
@@ -242,90 +270,67 @@ namespace LinqTests
         public void TakeWhileMoreThan300()
         {
             var employees = RepositoryFactory.GetEmployees();
-            var actual = employees.MyRealTakeWhile(e=>e.Age>30);
+            var actual = employees.MyRealTakeWhile(e => e.Age > 30);
 
             var expected = new List<Employee>
             {
                 new Employee{Name="Joe", Role=RoleType.Engineer, MonthSalary=100, Age=44, WorkingYear=2.6 } ,
                 new Employee{Name="Tom", Role=RoleType.Engineer, MonthSalary=140, Age=33, WorkingYear=2.6} ,
-                new Employee{Name="Kevin", Role=RoleType.Manager, MonthSalary=380, Age=55, WorkingYear=2.6} 
+                new Employee{Name="Kevin", Role=RoleType.Manager, MonthSalary=380, Age=55, WorkingYear=2.6}
             };
 
             expected.ToExpectedObject().ShouldEqual(actual.ToList());
         }
 
         [TestMethod]
-        public void Sum()
+        public void To91Port_IfContainstw()
         {
-            var products = RepositoryFactory.GetProducts();
-            var actual = products.MySum(p => p.Price);
+            var urls = RepositoryFactory.GetUrls();
+            var actual = WithoutLinq.ChangeTo91Port(urls);
 
-            var expected = 3650;
+            var expected = new List<string>
+            {
+                "http://tw.yahoo.com:91",
+                "https://facebook.com",
+                "https://twitter.com:91",
+                "http://github.com"
+            };
 
-            expected.ToExpectedObject().ShouldEqual(actual);
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
         }
-        [Ignore]
+
         [TestMethod]
-        public void Group_3_AndSum()
+        public void ToHttps()
         {
-            var products = RepositoryFactory.GetProducts();
-            var actual = products.GroupSum(3,p => p.Price);
+            var urls = RepositoryFactory.GetUrls();
+            var actual = urls.MySelect(urlItem => urlItem.Replace("http:", "https:").Replace(".com", ".com:8080"));
+
+            var expected = new List<string>
+            {
+                "https://tw.yahoo.com:8080",
+                "https://facebook.com:8080",
+                "https://twitter.com:8080",
+                "https://github.com:8080"
+            };
+
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+        }
+
+        [TestMethod]
+        public void UrlLength()
+        {
+            var urls = RepositoryFactory.GetUrls();
+            var actual = urls.MySelect(urlItem => urlItem.Length);
 
             var expected = new List<int>
             {
-                630,1530,1490
+                19,
+                20,
+                19,
+                17
             };
 
             expected.ToExpectedObject().ShouldEqual(actual.ToList());
-        }
-        [Ignore]
-        [TestMethod]
-        public void Group_5_AndSum()
-        {
-            var products = RepositoryFactory.GetProducts();
-            var actual = products.GroupSum(5, p => p.Price);
-
-            var expected = new List<int>
-            {
-                1550,2100
-            };
-
-            expected.ToExpectedObject().ShouldEqual(actual.ToList());
-        }
-        
-        [TestMethod]
-        public void AnyHasRecord()
-        {
-            var products = RepositoryFactory.GetProducts();
-            var actual = products.MyAny();
-
-            Assert.IsTrue(actual);
-
-        }
-
-        [TestMethod]
-        public void All()
-        {
-            var products = RepositoryFactory.GetProducts();
-            var actual = products.MyAll(p=>p.Price>200);
-
-            Assert.IsFalse(actual);
-
-        }
-
-        [TestMethod]
-        public void Distinct()
-        {
-            var employees = RepositoryFactory.GetEmployees();
-            var actual = employees.MySelect(p=>p.Role).MyDistinct();
-            var expected = new List<RoleType>
-            {
-                RoleType.Engineer,
-                RoleType.OP,
-                RoleType.Manager
-            };
-            expected.ToExpectedObject().ShouldEqual(actual.ToList());
-
         }
     }
 }
--- LinqTests/YourOwnLinq.cs
+++ LinqTests/YourOwnLinq.cs
@@ -3,15 +3,97 @@ using System.Collections.Generic;
 
 internal static class YourOwnLinq
 {
-    public static IEnumerable<TSource> MyWhere<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> func)
+    public static IEnumerable<int> GroupSum<TSource>(this IEnumerable<TSource> sources, int groupSize, Func<TSource, int> func)
     {
-        var result = source.GetEnumerator();
-        while (result.MoveNext())
+        var enumerator = sources.GetEnumerator();
+        var count = 1;
+        int sum = 0;
+        while (enumerator.MoveNext())
         {
-            var itme = result.Current;
-            if (func(itme))
+            sum += func(enumerator.Current);
+
+            if (count % groupSize == 0)
             {
-                yield return itme;
+                yield return sum;
+                sum = 0;
+            }
+            count++;
+        }
+        if (sum != 0)
+        {
+            yield return sum;
+        }
+    }
+
+    public static bool IsAnyEngeerAbove45<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+    {
+        foreach (var item in source)
+        {
+            if (predicate(item))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static bool MyAll<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+    {
+        var enumerator = source.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+            if (!predicate(enumerator.Current))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static bool MyAny<TSource>(this IEnumerable<TSource> sources)
+    {
+        return sources.GetEnumerator().MoveNext();
+    }
+
+    public static IEnumerable<TSource> MyDistinct<TSource>(this IEnumerable<TSource> sources)
+    {
+        var enumerator = sources.GetEnumerator();
+        var hashSet = new HashSet<TSource>();
+        while (enumerator.MoveNext())
+        {
+            var canAdd = hashSet.Add(enumerator.Current);
+            if (canAdd)
+            {
+                yield return enumerator.Current;
+            }
+        }
+    }
+
+    public static IEnumerable<TSource> MyRealSkipWhile<TSource>(this IEnumerable<TSource> sources, Func<TSource, bool> selector)
+    {
+        var enumerator = sources.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+            if (!selector(enumerator.Current))
+            {
+                yield return enumerator.Current;
+            }
+        }
+    }
+
+    public static IEnumerable<TSource> MyRealTakeWhile<TSource>(this IEnumerable<TSource> sources, Func<TSource, bool> predicate)
+    {
+        var enumerator = sources.GetEnumerator();
+
+        while (enumerator.MoveNext())
+        {
+            if (predicate(enumerator.Current))
+            {
+                yield return enumerator.Current;
+            }
+            else
+            {
+                yield break;
             }
         }
     }
@@ -21,6 +103,16 @@ internal static class YourOwnLinq
         foreach (var urlItem in urls)
         {
             yield return selector(urlItem);
+        }
+    }
+
+    public static IEnumerable<TResult> MySelect<TSource, TResult>(this IEnumerable<TSource> urls, Func<TSource, int, TResult> selector)
+    {
+        var count = 0;
+        foreach (var urlItem in urls)
+        {
+            yield return selector(urlItem, count);
+            count++;
         }
     }
 
@@ -54,26 +146,15 @@ internal static class YourOwnLinq
         }
     }
 
-    public static IEnumerable<TSource> MyRealSkipWhile<TSource>(this IEnumerable<TSource> sources, Func<TSource, bool> selector)
+    public static int MySum<TSource>(this IEnumerable<TSource> sources, Func<TSource, int> func)
     {
         var enumerator = sources.GetEnumerator();
+        int sum = 0;
         while (enumerator.MoveNext())
         {
-            if (!selector(enumerator.Current))
-            {
-                yield return enumerator.Current;
-            }
+            sum += func(enumerator.Current);
         }
-    }
-
-    public static IEnumerable<TResult> MySelect<TSource, TResult>(this IEnumerable<TSource> urls, Func<TSource, int, TResult> selector)
-    {
-        var count = 0;
-        foreach (var urlItem in urls)
-        {
-            yield return selector(urlItem, count);
-            count++;
-        }
+        return sum;
     }
 
     public static IEnumerable<TSource> MyTake<TSource>(this IEnumerable<TSource> sources, int topN)
@@ -108,85 +189,16 @@ internal static class YourOwnLinq
         }
     }
 
-    public static IEnumerable<TSource> MyRealTakeWhile<TSource>(this IEnumerable<TSource> sources, Func<TSource, bool> predicate)
+    public static IEnumerable<TSource> MyWhere<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> func)
     {
-        var enumerator = sources.GetEnumerator();
-
-        while (enumerator.MoveNext())
+        var result = source.GetEnumerator();
+        while (result.MoveNext())
         {
-            if (predicate(enumerator.Current))
+            var itme = result.Current;
+            if (func(itme))
             {
-                yield return enumerator.Current;
+                yield return itme;
             }
-            else
-            {
-                yield break;
-            }
-        }
-    }
-
-    public static bool MyAny<TSource>(this IEnumerable<TSource> sources)
-    {
-        return sources.GetEnumerator().MoveNext();
-    }
-
-    public static bool MyAll<TSource>(this IEnumerable<TSource> source,Func<TSource,bool> predicate)
-    {
-        var enumerator = source.GetEnumerator();
-        while (enumerator.MoveNext())
-        {
-            if (!predicate(enumerator.Current))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static int MySum<TSource>(this IEnumerable<TSource> sources, Func<TSource, int> func)
-    {
-        var enumerator = sources.GetEnumerator();
-        int sum = 0;
-        while (enumerator.MoveNext())
-        {
-            sum += func(enumerator.Current);
-        }
-        return sum;
-    }
-
-    public static IEnumerable<TSource> MyDistinct<TSource>(this IEnumerable<TSource> sources)
-    {
-        var enumerator = sources.GetEnumerator();
-        var hashSet = new HashSet<TSource>();
-        while (enumerator.MoveNext())
-        {
-            var canAdd = hashSet.Add(enumerator.Current);
-            if (canAdd)
-            {
-                yield return enumerator.Current;
-            }
-        }
-    }
-    public static IEnumerable<int> GroupSum<TSource>(this IEnumerable<TSource> sources,int groupSize, Func<TSource, int> func)
-    {
-        var enumerator = sources.GetEnumerator();
-        var count = 1;
-        int sum = 0;
-        while (enumerator.MoveNext())
-        {
-            sum += func(enumerator.Current);
-
-            
-            if (count%groupSize==0)
-            {
-                yield return sum;
-                sum = 0;
-            }
-            count++;
-        }
-        if (sum != 0)
-        {
-            yield return sum;
         }
     }
 }
